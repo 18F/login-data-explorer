e4bb5db94e150412f9160c7386ab6b6d808efa06
Moncef Belyamani <moncef.belyamani@gsa.gov>
2018-11-13 15:40:25 -0500

    Add ability to search events by email address
    In order to get a specific user's info by their email address, we have to be able to convert their email address to the `email_fingerprint`, and then use the fingerprint in the SQL query. For example, to get a user's events, the SQL would be:
    ```
    SELECT events.event_type, events.created_at FROM events LEFT JOIN users ON events.user_id = users.id WHERE users.email_fingerprint = [fingerprint];
    ```
    
    **How**:
    1. Go to the `/blazer` endpoint and click the "New Query" button in the top right
    
    2. In the SQL entry box on the left, enter:
    ```
    SELECT events.event_type, events.created_at FROM events LEFT JOIN users ON events.user_id = users.id WHERE users.email_fingerprint = {email_fingerprint};
    ```
    This makes use of Blazer's variable queries:
    https://github.com/ankane/blazer#variables
    
    3. Run `rails generate controller EmailFingerprint index create` to generate a quick scaffold with routes, controller, and views.
    
    4. Add a form that submits an email parameter, and the controller converts the email to the fingerprint, then redirects to the query that was created in step 2. Note that the redirect URL depends on the id of the query.
    
    5. The `hmac_fingerprinter_key` on the data explorer app must match the one on the IdP server that is being queried. To edit the app's credentials, run `rails credentials:edit`.

---
+++ app/controllers/email_fingerprint_controller.rb
@@ -0,0 +1,18 @@
+class EmailFingerprintController < ApplicationController
+  def index
+  end
+
+  def create
+    @email = params[:email]
+    @fingerprint = fingerprint_for(@email)
+    redirect_to "/blazer/queries/1-events-for-specific-user?email_fingerprint=#{@fingerprint}"
+  end
+
+  private
+
+  def fingerprint_for(email)
+    digest = OpenSSL::Digest::SHA256.new
+    key = Rails.application.credentials.hmac_fingerprinter_key
+    OpenSSL::HMAC.hexdigest(digest, key, email)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   mount Blazer::Engine, at: 'blazer'
+  get 'email_fingerprint' => 'email_fingerprint#index'
+  post 'email_fingerprint' => 'email_fingerprint#create'
 end
