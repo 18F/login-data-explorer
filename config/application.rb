4a429890c6137d724f6e78bde715c50844a7328b
Moncef Belyamani <moncef.belyamani@gsa.gov>
2018-11-13 12:29:28 -0500

    Replace Identity with Login
    **Why**: We want our repos to have the `login` prefix instead of `identity`.

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ require "action_view/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module IdentityDataExplorer
+module LoginDataExplorer
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
